6c5a19e1d221ca5cf2e80dbb71ef563d179d8e01
Aman Srivastava <aman.srivastava2639@gmail.com>
2024-04-25 16:21:53 +0530

    Add files via upload

--- src/Component/Gallery.jsx
+++ src/Component/Gallery.jsx
@@ -34,11 +34,6 @@ const Gallery = () => {
   <section className="section-padding">
     <div className="container">
       <div className="row">
-        {/* <div className="col-md-12">
-          <div className="section-subtitle">Images</div>
-          <div className="section-title">Image Gallery</div>
-        </div> */}
-        {/* 3 columns */}
         <div className="col-md-4 gallery-item">
           <a href="img/images/Business class edited/IMG_3104.1 - Copy.JPG" title="" className="img-zoom">
             <div className="gallery-box">
@@ -82,7 +77,7 @@ const Gallery = () => {
           </a>
         </div>
         {/* 2 columns */}
-        <div className="col-md-6 gallery-item">
+        <div className="col-md-4 gallery-item">
           <a href="img/images/Premium Economy/8.webp" title="" className="img-zoom">
             <div className="gallery-box">
               <div className="gallery-img">
@@ -96,7 +91,7 @@ const Gallery = () => {
             </div>
           </a>
         </div>
-        <div className="col-md-6 gallery-item">
+        <div className="col-md-4 gallery-item">
           <a href="img/images/Maharaja Suite Edited/17-1-min.webp" title="" className="img-zoom">
             <div className="gallery-box">
               <div className="gallery-img">
@@ -146,6 +141,110 @@ const Gallery = () => {
                 {" "}
                 <img
                   src="img/slider/banner-4.webp"
+                  className="img-fluid mx-auto d-block"
+                  alt="work-img"
+                />{" "}
+              </div>
+            </div>
+          </a>
+        </div>
+
+        {/*3 Column */}
+        <div className="col-md-4 gallery-item">
+          <a href="img/images/Family Suite Edited/4.webp" title="" className="img-zoom">
+            <div className="gallery-box">
+              <div className="gallery-img">
+                {" "}
+                <img
+                  src="img/images/Family Suite Edited/4.webp"
+                  className="img-fluid mx-auto d-block"
+                  alt="work-img"
+                />{" "}
+              </div>
+            </div>
+          </a>
+        </div>
+        <div className="col-md-4 gallery-item">
+          <a href="img/images/Premium Economy/13-1.png" title="" className="img-zoom">
+            <div className="gallery-box">
+              <div className="gallery-img">
+                {" "}
+                <img
+                  src="img/images/Premium Economy/13-1.png"
+                  className="img-fluid mx-auto d-block"
+                  alt="work-img"
+                />{" "}
+              </div>
+            </div>
+          </a>
+        </div>
+        <div className="col-md-4 gallery-item">
+          <a href="img/images/Business class edited/jpeg-optimizer_IMG_3107.JPG" title="" className="img-zoom">
+            <div className="gallery-box">
+              <div className="gallery-img">
+                {" "}
+                <img
+                  src="img/images/Business class edited/jpeg-optimizer_IMG_3107.JPG"
+                  className="img-fluid mx-auto d-block"
+                  alt="work-img"
+                />{" "}
+              </div>
+            </div>
+          </a>
+        </div>
+        
+        {/* 2 columns */}
+        
+        <div className="col-md-4 gallery-item">
+          <a href="img/images/banquet/jpeg-optimizer_IMG_2345.jpg" title="" className="img-zoom">
+            <div className="gallery-box">
+              <div className="gallery-img">
+                {" "}
+                <img
+                  src="img/images/banquet/jpeg-optimizer_IMG_2345.jpg"
+                  className="img-fluid mx-auto d-block"
+                  alt="work-img"
+                />{" "}
+              </div>
+            </div>
+          </a>
+        </div>
+        <div className="col-md-4 gallery-item">
+          <a href="img/images/restaurant/12.webp" title="" className="img-zoom">
+            <div className="gallery-box">
+              <div className="gallery-img">
+                {" "}
+                <img
+                  src="img/images/restaurant/12.webp"
+                  className="img-fluid mx-auto d-block"
+                  alt="work-img"
+                />{" "}
+              </div>
+            </div>
+          </a>
+        </div>
+        
+        <div className="col-md-4 gallery-item">
+          <a href="img/images/banquet/IMG_2093.1.jpg" title="" className="img-zoom">
+            <div className="gallery-box">
+              <div className="gallery-img">
+                {" "}
+                <img
+                  src="img/images/banquet/IMG_2093.1.jpg"
+                  className="img-fluid mx-auto d-block"
+                  alt="work-img"
+                />{" "}
+              </div>
+            </div>
+          </a>
+        </div>
+        <div className="col-md-4 gallery-item">
+          <a href="img/images/restaurant/2-Copy.png" title="" className="img-zoom">
+            <div className="gallery-box">
+              <div className="gallery-img">
+                {" "}
+                <img
+                  src="img/images/restaurant/2-Copy.png"
                   className="img-fluid mx-auto d-block"
                   alt="work-img"
                 />{" "}
